bd68a5cab090114f3c5f3b9009a2f1f22c7b0f00
dsaban <dsaban@amadeus.com>
2020-03-04 22:28:58 +0100

    Best version so far. Landscape cards a bit too small

--- src/main/ruby/hexagons.rb
+++ src/main/ruby/hexagons.rb
@@ -18,8 +18,7 @@ Squib::Deck.new(cards: deck["Chapitre"].size(),
   polygon layout: :bleed
   polygon layout: :cut
 #  polygon layout: :outline
-
-  
+ 
   %w(1 4).each do |key|
     polygon layout: "Vent"+key+"Icone", n: 20, angle: (key.to_i-1)*3.14159/3
   end
@@ -36,9 +35,27 @@ Squib::Deck.new(cards: deck["Chapitre"].size(),
 
   %w(Incolor Red Green Trick).each do |key|
 #    rect layout: "Terrain"+key, radius: 0, stroke_color: '#00000000'
-    text str: deck[key], layout: "Terrain"+key
+#    text str: deck[key], layout: "Terrain"+key
   end
 
+  %w(RGI1 RGI2 RGI3 T1 T2 T3 T4).each do |key|
+    png file: deck[key].map{ |c| 
+    if (c == nil)
+      "src/resources/helpers/d6-empty.png"
+    elsif (c == "R")
+      "src/resources/helpers/d6-red.png" 
+    elsif (c == "G")
+      "src/resources/helpers/d6-green.png" 
+    elsif (c == "I")
+      "src/resources/helpers/d6-white.png" 
+    elsif (c == "T")
+      "src/resources/helpers/d6-black.png" 
+    else
+       "src/resources/helpers/d6-empty.png"
+    end
+    },
+  layout: "Terrain"+key
+  end
 
 
   %w(Description).each do |key|
