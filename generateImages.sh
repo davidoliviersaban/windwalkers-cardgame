17bc0bd2691f0bd5ab8bd7c31510330b6320166a
dsaban <dsaban@amadeus.com>
2020-06-07 16:11:06 +0200

    Adapting generate images

--- generateImages.sh
+++ generateImages.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
-rm -rf _cards* _terrain
-ruby src/main/ruby/land_cards.rb
-ruby src/main/ruby/horde_cards.rb
-ruby src/main/ruby/gliders_cards.rb
+#rm -rf _cards* _terrain
+#ruby src/main/ruby/land_cards.rb
+#ruby src/main/ruby/horde_cards.rb
+#ruby src/main/ruby/gliders_cards.rb
 cd ../printableCardsAppender
-./gradlew run --args="../windwalkers-cardgame/_terrain ../windwalkers-cardgame/imagesToPrint/terrain A4 true"
-./gradlew run --args="../windwalkers-cardgame/_cards1   ../windwalkers-cardgame/imagesToPrint/cards_v1_ A4 false"
-#./gradlew run --args="../windwalkers-cardgame/_cards2   ../windwalkers-cardgame/imagesToPrint/cards_v2_ A4 false"
-./gradlew run --args="../windwalkers-cardgame/_cardsG1   ../windwalkers-cardgame/imagesToPrint/cards_g1_ A4 false"
+./gradlew appendCard --args="../windwalkers-cardgame/_terrain ../windwalkers-cardgame/imagesToPrint/terrain A4 true"
+./gradlew appendCard --args="../windwalkers-cardgame/_cards1   ../windwalkers-cardgame/imagesToPrint/cards_v1_ A4 false"
+#./gradlew appendCard --args="../windwalkers-cardgame/_cards2   ../windwalkers-cardgame/imagesToPrint/cards_v2_ A4 false"
+./gradlew appendCard --args="../windwalkers-cardgame/_cardsG1   ../windwalkers-cardgame/imagesToPrint/cards_g1_ A4 false"
