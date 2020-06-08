48f1883add6d8a6ab7d86eaf3d48c91e11ed8ec0
dsaban <dsaban@amadeus.com>
2020-06-08 09:47:49 +0200

    Description field higher, moved up the dices as well, removed useless code

--- generateImages.sh
+++ generateImages.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
+
+set -o errexit -o noclobber -o pipefail
+
 rm -rf _cards* _terrain
 ruby src/main/ruby/land_cards.rb
 ruby src/main/ruby/horde_cards.rb
 cd ../printableCardsAppender
 ./gradlew appendCard --args="../windwalkers-cardgame/_terrain ../windwalkers-cardgame/imagesToPrint/terrain A4 true"
 ./gradlew appendCard --args="../windwalkers-cardgame/_cards1   ../windwalkers-cardgame/imagesToPrint/cards_v1_ A4 false"
-#./gradlew run --args="../windwalkers-cardgame/_cards2   ../windwalkers-cardgame/imagesToPrint/cards_v2_ A4 false"
