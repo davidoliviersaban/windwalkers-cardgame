ee44e902e88f8ff8233112ed96fd4e38cdd5ee80
dsaban <dsaban@amadeus.com>
2020-09-29 09:16:59 +0200

    Regle 2 joueurs ajoutee

--- src/main/ruby/horde_cards.rb
+++ src/main/ruby/horde_cards.rb
@@ -2,8 +2,8 @@ require 'squib'
 
 deck1 = Squib.xlsx file: %w(src/resources/horde_cards.xlsx)
 #deck1 = Squib.csv file: %w(src/resources/horde_cards.csv)
-deck2 = Squib.csv file: %w(src/resources/horde2_cards.csv)
-deck_Vs = Squib.csv file: %w(src/resources/horde_vs_cards.csv)
+#deck2 = Squib.csv file: %w(src/resources/horde2_cards.csv)
+#deck_Vs = Squib.csv file: %w(src/resources/horde_vs_cards.csv)
 
 def drawCards(deck,dirname)
 #  rect layout: :bleed
@@ -35,7 +35,9 @@ def drawCards(deck,dirname)
     text str: deck[key], layout: key
   end
 
-  png layout: deck["Position"].map{ |pos| pos+"Icon"}
+  png layout: deck["Position"].map{ |pos| ""+pos.to_s+"Icon"}
+
+  png layout: deck["Tier"].map{ |ext|  "T"+ext.to_s+"Icon"  }
 
   png file: deck["Extension"].map{ |ext|
     if (ext != nil)
@@ -45,37 +47,25 @@ def drawCards(deck,dirname)
     end
   }, layout: "ExtensionIcon"
 
-  png file: deck["Extension"].map{ |ext|
-    if (ext != nil)
-      "src/resources/helpers/"+ext+".png"
-    else
-      "src/resources/helpers/d6-empty.png"
-    end
-  }, layout: "ExtensionIcon"
-
-  png layout: deck["Tier"].map{ |tier|
-    tier+'Icon'
-  }
-
-  save_png prefix: deck["Id"].zip(deck["Position"],deck["Nom"]).map{|name| "%03d.%s.%s"%name}, dir: dirname#dir: '_cards'
+  save_png prefix: deck["Id"].zip(deck["Position"],deck["Nom"],deck["Tier"]).map{|name| "%03d.%s.%s.T%s."%name}, dir: dirname#dir: '_cards'
 
 end
 
 
-Squib::Deck.new(cards: deck1["Nom"].size,#cards: deck["Name"].size, # cards: 1,#
+Squib::Deck.new(cards: deck1["Nom"].size,
                 layout: %w(src/resources/Vlayout.yml src/resources/Vcards.yml),
                 width: '2.5in', height: '3.5in') do
   drawCards(deck1,'_cards1')
 end
 
-Squib::Deck.new(cards: deck2["Nom"].size,#cards: deck["Name"].size, # cards: 1,#
-                layout: %w(src/resources/Vlayout.yml src/resources/Vcards.yml),
-                width: '2.5in', height: '3.5in') do
-  drawCards(deck2,'_cards2')
-end
+# Squib::Deck.new(cards: deck2["Nom"].size,
+#                 layout: %w(src/resources/Vlayout.yml src/resources/Vcards.yml),
+#                 width: '2.5in', height: '3.5in') do
+#   drawCards(deck2,'_cards2')
+# end
 
-Squib::Deck.new(cards: deck_Vs["Nom"].size,#cards: deck["Name"].size, # cards: 1,#
-                layout: %w(src/resources/Vlayout.yml src/resources/Vcards.yml),
-                width: '2.5in', height: '3.5in') do
-  drawCards(deck_Vs,'_cards_vs')
-end
+# Squib::Deck.new(cards: deck_Vs["Nom"].size,
+#                 layout: %w(src/resources/Vlayout.yml src/resources/Vcards.yml),
+#                 width: '2.5in', height: '3.5in') do
+#   drawCards(deck_Vs,'_cards_vs')
+# end
