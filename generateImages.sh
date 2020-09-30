9fb3b8a4b429a25ca7c7b296d2743b8c82c5103f
dsaban <dsaban@amadeus.com>
2020-09-30 11:05:32 +0200

    fix generateImages plus compatible..

--- generateImages.sh
+++ generateImages.sh
@@ -17,10 +17,10 @@ function install_rbenv_upgrade_ruby() {
 source ~/.bash_profile
 
 rm -rf _cards* _terrain
-ruby src/main/ruby/land_cards.rb
+#ruby src/main/ruby/land_cards.rb
 ruby src/main/ruby/horde_cards.rb
 cd ../printableCardsAppender
-./gradlew run --args="../windwalkers-cardgame/_terrain ../windwalkers-cardgame/imagesToPrint/terrain A4 true"
-./gradlew run --args="../windwalkers-cardgame/_cards1   ../windwalkers-cardgame/imagesToPrint/cards_v1_ A4 false"
-./gradlew run --args="../windwalkers-cardgame/_cards2   ../windwalkers-cardgame/imagesToPrint/cards_v2_ A4 false"
-./gradlew run --args="../windwalkers-cardgame/_cards_vs   ../windwalkers-cardgame/imagesToPrint/cards_vs_ A4 false"
+#./gradlew appendCard --args="../windwalkers-cardgame/_terrain ../windwalkers-cardgame/imagesToPrint/terrain   A4 true"
+./gradlew appendCard --args="../windwalkers-cardgame/_cards1  ../windwalkers-cardgame/imagesToPrint/cards_v1_ A4 false"
+#./gradlew appendCard --args="../windwalkers-cardgame/_cards2   ../windwalkers-cardgame/imagesToPrint/cards_v2_ A4 false"
+#./gradlew appendCard --args="../windwalkers-cardgame/_cards_vs   ../windwalkers-cardgame/imagesToPrint/cards_vs_ A4 false"
