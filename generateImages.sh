9c9988e77220e07628fd6b3dd7103003d320c18e
David-Olivier Saban <davidolivier.saban@gmail.com>
2020-10-31 09:11:34 +0100

    Generation of cards back cover

--- generateImages.sh
+++ generateImages.sh
@@ -21,21 +21,10 @@ function install_rbenv_upgrade_ruby() {
 
 source ~/.bash_profile
 
-<<<<<<< HEAD
 rm -rf _cards* _terrain*
-#ruby src/main/ruby/land_cards.rb
+ruby src/main/ruby/land_cards.rb
 ruby src/main/ruby/horde_cards.rb
 cd ../printableCardsAppender
 ./gradlew appendCard --args="../windwalkers-cardgame/_terrain ../windwalkers-cardgame/imagesToPrint/terrain   A4 true"
 ./gradlew appendCard --args="../windwalkers-cardgame/_cards1  ../windwalkers-cardgame/imagesToPrint/cards_v1_ A4 false"
 ./gradlew appendCard --args="../windwalkers-cardgame/_cards_cut  ../windwalkers-cardgame/imagesToPrint/cards_cut_ A4 false"
-=======
-rm -rf _cards* _terrain
-#ruby src/main/ruby/land_cards.rb
-ruby src/main/ruby/horde_cards.rb
-cd ../printableCardsAppender
-#./gradlew appendCard --args="../windwalkers-cardgame/_terrain ../windwalkers-cardgame/imagesToPrint/terrain   A4 true"
-./gradlew appendCard --args="../windwalkers-cardgame/_cards1  ../windwalkers-cardgame/imagesToPrint/cards_v1_ A4 false"
->>>>>>> 9fb3b8a4b429a25ca7c7b296d2743b8c82c5103f
-#./gradlew appendCard --args="../windwalkers-cardgame/_cards2   ../windwalkers-cardgame/imagesToPrint/cards_v2_ A4 false"
-#./gradlew appendCard --args="../windwalkers-cardgame/_cards_vs   ../windwalkers-cardgame/imagesToPrint/cards_vs_ A4 false"
